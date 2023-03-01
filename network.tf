d320905894e00eafb5706e03451519b2a80be537
paul jobb <paul.jobb@gmail.com>
2023-03-01 20:54:53 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,7 +1,8 @@
 
 
 module "network" {
-  source              = "Azure/network/azurerm"
+  source              = "app.terraform.io/paul-jobb-training/network/azurerm"
+  version             = "3.5.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
